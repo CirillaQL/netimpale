f4aa0a54da087f2f0ed7a665e709fece700a63d2
lu.qian <lu.qian@easystack.cn>
2021-11-19 17:09:10 +0800

    针对连接进行包装，添加HTTPConn

--- server/http.go
+++ server/http.go
@@ -1,12 +1,28 @@
 package server
 
-import "netimpale/utils/log"
+import (
+	"net"
+	"netimpale/utils/log"
+	"time"
+)
 
 var LOG = log.LOG
 
 // HTTPManager Http连接的管理结构体，目前用来处理Http连接的相关请求
 type HTTPManager struct {
-	RunID string
+	RunID    string
+	Conns    []*HTTPConn
+	Listener net.Listener
+}
+
+// HTTPConn 针对HTTP的Conn,添加相关的支持
+type HTTPConn struct {
+	ConnID string
+	Token  string
+	Conn   *net.Conn
+	Data   []byte
+	Status uint32
+	Time   time.Time
 }
 
 // NewHTTPManager 创建HTTPManager
@@ -14,7 +30,19 @@ func NewHTTPManager() *HTTPManager {
 	return &HTTPManager{}
 }
 
+// ConnHandler 针对连接进行处理的Handler
+func (http *HTTPManager) ConnHandler(conn net.Conn) {
+
+}
+
 // Run 运行
 func (http *HTTPManager) Run() {
+	for {
+		conn, err := http.Listener.Accept()
+		if err != nil {
+			LOG.Errorf("HTTPManager Accept Failed. HTTPManger RunID: %s, Error: %v", http.RunID, err)
+		}
+		go http.ConnHandler(conn)
 
+	}
 }
--- server/server.go
+++ server/server.go
@@ -2,4 +2,5 @@ package server
 
 // Server 服务器结构体
 type Server struct {
+	manager *HTTPManager
 }
