2faeb4cb5231be03f0aa41048f87c82b0f414bfb
lu.qian <lu.qian@easystack.cn>
2022-01-11 14:42:28 +0800

    加载yaml配置文件与格式化输出的方法

--- main.go
+++ main.go
@@ -1,54 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"netimpale/utils/config"
-	"netimpale/utils/log"
-
-	"gopkg.in/yaml.v3"
 )
 
-var LOG = log.LOG
-
 func main() {
-	// 创建 listener
-	//listener, err := net.Listen("tcp", "localhost:9090")
-	//if err != nil {
-	//	fmt.Println("Error listening", err.Error())
-	//	return //终止程序
-	//}
-	//// 监听并接受来自客户端的连接
-	//for {
-	//	conn, err := listener.Accept()
-	//	if err != nil {
-	//		fmt.Println("Error accepting", err.Error())
-	//		return // 终止程序
-	//	}
-	//	go doServerStuff(conn)
-	//}
-	conf := new(config.Client)
-	yamlFile, err := ioutil.ReadFile("./assets/client.yaml")
-	if err != nil {
-		LOG.Errorf("%v", err)
-	}
-	err = yaml.Unmarshal(yamlFile, conf)
-	if err != nil {
-		LOG.Errorf("%v", err)
-	}
-	fmt.Println(conf.TCP.Host)
-	fmt.Printf("%v", conf)
+	clientConfig := config.LoadClientConfig("./assets/client.yaml")
+	clientConfig.OutputClientConfig()
+	serverConfig := config.LoadServerConfig("./assets/server.yaml")
+	serverConfig.OutputServerConfig()
 }
-
-//func doServerStuff(conn net.Conn) {
-//	for {
-//		buf := make([]byte, 512)
-//		len, err := conn.Read(buf)
-//		if err != nil {
-//			fmt.Println("Error reading", err.Error())
-//			return //终止程序
-//		}
-//		//fmt.Printf("%v", string(buf[:len]))
-//		network_utils.GenerateHTTPRequest(buf, len)
-//	}
-//}
--- utils/config/yaml.go
+++ utils/config/yaml.go
@@ -1,26 +1,34 @@
 package config
 
 import (
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
 	"netimpale/utils/log"
 	"os"
 )
 
 var LOG = log.LOG
 
+type TCP struct {
+	Host string `yaml:"host"`
+	Port uint32 `yaml:"port"`
+}
+
+type UDP struct {
+	Host string `yaml:"host"`
+	Port uint32 `yaml:"port"`
+}
+
 // Client Config 客户端
 type Client struct {
-	TCP struct {
-		Host string `yaml:"host"`
-		Port uint32 `yaml:"port"`
-	}
-	UDP struct {
-		Host string `yaml:"host"`
-		Port uint32 `yaml:"port"`
-	}
+	TCP
+	UDP
 }
 
 // Server Config 服务端
 type Server struct {
+	TCP
+	UDP
 }
 
 // 判断对应文件是否存在
@@ -34,10 +42,66 @@ func checkConfigFileIsExist(filepath string) bool {
 
 // LoadClientConfig 加载客户端配置文件
 func LoadClientConfig(filepath string) *Client {
-	return nil
+	// 首先检查配置文件是否存在
+	exist := checkConfigFileIsExist(filepath)
+	if exist != true {
+		LOG.Errorf("Config file doesn't exit: %v", filepath)
+	}
+	// 读文件
+	yamlFile, err := ioutil.ReadFile(filepath)
+	if err != nil || yamlFile == nil {
+		LOG.Errorf("Can't Open yaml file. Error: %v", err)
+	}
+	// 加载对应的配置
+	clientConfig := new(Client)
+	err = yaml.Unmarshal(yamlFile, clientConfig)
+	if err != nil {
+		LOG.Errorf("Can't load ")
+	}
+	return clientConfig
 }
 
 // LoadServerConfig 加载服务端配置文件
 func LoadServerConfig(filepath string) *Server {
-	return nil
+	// 首先检查配置文件是否存在
+	exist := checkConfigFileIsExist(filepath)
+	if exist != true {
+		LOG.Errorf("Config file doesn't exit: %v", filepath)
+	}
+	// 读文件
+	yamlFile, err := ioutil.ReadFile(filepath)
+	if err != nil || yamlFile == nil {
+		LOG.Errorf("Can't Open yaml file. Error: %v", err)
+	}
+	// 加载对应的配置
+	serverConfig := new(Server)
+	err = yaml.Unmarshal(yamlFile, serverConfig)
+	if err != nil {
+		LOG.Errorf("Can't load ")
+	}
+	return serverConfig
+}
+
+// OutputClientConfig 格式化输出客户端证书的内容
+func (client *Client) OutputClientConfig() {
+	if client.TCP != (TCP{}) {
+		LOG.Infof("TCP Host: %v", client.TCP.Host)
+		LOG.Infof("TCP Port: %v", client.TCP.Port)
+	}
+	if client.UDP != (UDP{}) {
+		LOG.Infof("UDP Host: %v", client.UDP.Host)
+		LOG.Infof("UDP Port: %v", client.UDP.Port)
+	}
+}
+
+// OutputServerConfig 格式化输出服务端证书的内容
+func (server *Server) OutputServerConfig() {
+	if server.TCP != (TCP{}) {
+		LOG.Infof("TCP Host: %v", server.TCP.Host)
+		LOG.Infof("TCP Port: %v", server.TCP.Port)
+	}
+	if server.UDP != (UDP{}) {
+		LOG.Infof("UDP Host: %v", server.UDP.Host)
+		LOG.Infof("UDP Port: %v", server.UDP.Port)
+	}
 }
