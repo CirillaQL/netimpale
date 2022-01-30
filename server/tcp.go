27db8737846f0ce44a323ebe074f0599f287a738
lu.qian <lu.qian@easystack.cn>
2022-01-30 14:08:40 +0800

    重新调整TCPManager

--- server/tcp.go
+++ server/tcp.go
@@ -1,66 +1,32 @@
 package server
 
-import (
-	"net"
-	"netimpale/server/proxy"
-	"time"
-)
+import "net"
 
 // TCPManager TCP连接的管理结构体，目前用来处理TCP连接的相关请求
 type TCPManager struct {
-	proxy          *proxy.TCPProxy
-	clientAddr     *net.TCPAddr
-	clientListener *net.TCPListener
-
-	clientCh chan *net.TCPConn
+	RemoteListener *net.TCPListener //外部TCP的监听器
 }
 
 // NewTCPManager 创建TCPManager
-func NewTCPManager(network, ServerAddr, ClientAddr string) *TCPManager {
-	LOG.Infof("Create TCPManager")
-	tcpProxy := proxy.NewTCPProxy(network, ServerAddr)
-	tcpAddr, err := net.ResolveTCPAddr(network, ClientAddr)
+func NewTCPManager(serverAddr string) *TCPManager {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", serverAddr)
 	if err != nil {
-		LOG.Errorf("Create TCPManager TCPClientAddr Failed: %v", err)
+		LOG.Errorf("Create TCPManager TCPAddr Failed. Error: %v", err)
 	}
-	tcpListener, err := net.ListenTCP(network, tcpAddr)
+	tcpListener, err := net.ListenTCP("tcp", tcpAddr)
 	if err != nil {
-		LOG.Errorf("Create TCPManager TCPClientListener Failed: %v", err)
+		LOG.Errorf("Create TCPManager TCPListener Failed. Error: %v", err)
 	}
-	return &TCPManager{
-		proxy:          tcpProxy,
-		clientAddr:     tcpAddr,
-		clientListener: tcpListener,
-		clientCh:       make(chan *net.TCPConn),
-	}
+	return &TCPManager{RemoteListener: tcpListener}
 }
 
-// ConnectClient 连接客户端
-func (tcp *TCPManager) ConnectClient() {
-	for {
-		remoteConn, err := tcp.clientListener.AcceptTCP()
-		if err != nil {
-			LOG.Errorf("TCPProxy Can't Handle Connect: %v", err)
-		}
-		err = remoteConn.SetKeepAlive(true)
-		if err != nil {
-			LOG.Errorf("TCPProxy Can't Handle Connect: %v", err)
-		}
-		err = remoteConn.SetKeepAlivePeriod(2 * time.Second)
-		if err != nil {
-			LOG.Errorf("TCPProxy Can't Handle Connect: %v", err)
-		}
-		go func(ch chan *net.TCPConn, Conn *net.TCPConn) {
-			ch <- Conn
-		}(tcp.clientCh, remoteConn)
-	}
+// Start 启动服务端TCPManager，主要开始维护与客户端的连接
+func (tcpManager *TCPManager) Start() {
+	// TODO：从与客户端的TCP连接池中取出对应的TCPConn
+
 }
 
-// Run 运行转发
-func (tcp *TCPManager) Run() {
-	//首先运行Proxy的StartListener，用来Handle 远程TCP连接
-	go tcp.proxy.StartListener()
-	//此时需获取来自客户端的net.TCPConn
-	go tcp.ConnectClient()
-	go tcp.proxy.HandleTCPConnection(tcp.clientCh)
+// Run 执行转发
+func (tcpManager *TCPManager) Run() {
+
 }
